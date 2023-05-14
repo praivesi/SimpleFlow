82c4c9ef93376c49271992591638e2993fc8439f
praivesi <praivesi@gmail.com>
2023-05-15 00:56:11 +0900

    Initial Commit

---
+++ SimpleFlow/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SimpleFlow
+//
+//  Created by expDemian on 2023/05/15.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
